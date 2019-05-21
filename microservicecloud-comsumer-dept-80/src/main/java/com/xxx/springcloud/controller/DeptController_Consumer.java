66a84f0884936f1397e88c7a902ad375a8416adb
gexx <944644308@qq.com>
2019-05-22 01:14:05 +0800

    ribbon负载均衡

--- microservicecloud-comsumer-dept-80/src/main/java/com/xxx/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-comsumer-dept-80/src/main/java/com/xxx/springcloud/controller/DeptController_Consumer.java
@@ -13,8 +13,8 @@ import com.xxx.springcloud.entities.Dept;
 @RestController
 public class DeptController_Consumer {
 
-	private static final String REST_URL_PREFIX = "http://localhost:8001";
-//	private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
+//	private static final String REST_URL_PREFIX = "http://localhost:8001";
+	private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
 
 	/**
 	 * 使用 使用restTemplate访问restful接口非常的简单粗暴无脑。 (url, requestMap,
---
+++ microservicecloud-provider-dept-8002/src/main/java/com/xxx/springcloud/service/impl/DeptServiceImpl.java
@@ -0,0 +1,32 @@
+package com.xxx.springcloud.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.xxx.springcloud.dao.DeptDao;
+import com.xxx.springcloud.entities.Dept;
+import com.xxx.springcloud.service.DeptService;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+	@Autowired
+	private DeptDao dao;
+
+	@Override
+	public boolean add(Dept dept) {
+		return dao.addDept(dept);
+	}
+
+	@Override
+	public Dept get(Long id) {
+		return dao.findById(id);
+	}
+
+	@Override
+	public List<Dept> list() {
+		return dao.findAll();
+	}
+
+}

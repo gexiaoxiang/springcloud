b36c0f33dadf565d886e98241322a87c1c451e42
gexx <944644308@qq.com>
2019-05-22 23:35:33 +0800

    自定义bibbon算法

--- microservicecloud-comsumer-dept-80/src/main/java/com/xxx/springcloud/cfgbeans/ConfigBeans.java
+++ microservicecloud-comsumer-dept-80/src/main/java/com/xxx/springcloud/cfgbeans/ConfigBeans.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+
 @Configuration
 public class ConfigBeans {
 
@@ -12,5 +17,11 @@ public class ConfigBeans {
 	@LoadBalanced //springcloud ribbon实现负载均衡
 	public RestTemplate getreRestTemplate() {
 		return new RestTemplate();
+	}
+	@Bean
+	public IRule myRule() {
+		//用随机算法代替轮询
+//		return new RandomRule();
+		return new RetryRule();
 	}
 }
